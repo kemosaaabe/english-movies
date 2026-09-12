4729e9be6d9ef953964d63a790579b09edf76622
kemosaaabe <sanchellos27@mail.ru>
2026-09-12 22:22:11 +0400

    feat: update react router

--- apps/frontend/src/app/router/index.tsx
+++ apps/frontend/src/app/router/index.tsx
@@ -1,14 +1,25 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import { ExercisePage } from '@pages/exercise';
 import { UploadPage } from '@pages/upload';
 import { routes } from './constants';
 
-export const AppRouter = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={routes.upload} element={<UploadPage />} />
-      <Route path={routes.exercise} element={<ExercisePage />} />
-      <Route path="*" element={<Navigate to={routes.upload} replace />} />
-    </Routes>
-  </BrowserRouter>
-);
+const router = createBrowserRouter([
+  {
+    path: routes.upload,
+    Component: UploadPage,
+  },
+  {
+    path: routes.exercise,
+    Component: ExercisePage,
+  },
+  {
+    path: '*',
+    element: <Navigate to={routes.upload} replace />,
+  },
+]);
+
+export const AppRouter = () => <RouterProvider router={router} />;
